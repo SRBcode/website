b210427321783ac4ca60d4cc21eb233780cb0aae
Aleksandar Milosevic <alexmilosevic992@gmail.com>
2021-06-19 20:25:18 +0200

    js and html manipulation testing

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 let test = function(){
     conole.log('test');
 };
+
+document.getElementById("test").addEventListener("mouseover", mouseOver);
+function mouseOver(){
+    document.getElementById("test").style.color = "red";
+}
+
