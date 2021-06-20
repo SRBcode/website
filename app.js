417584bc4301f5192bb0eb242d5a81310d973438
Aleksandar Milosevic <alexmilosevic992@gmail.com>
2021-06-21 00:32:30 +0200

    js testing

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let test = function(){
     conole.log('test');
 };
 
-document.getElementById("test").addEventListener("mouseover", mouseOver);
+document.getElementById("test").onmouseover = function() {mouseOver();}
 function mouseOver(){
     document.getElementById("test").style.color = "red";
 }
